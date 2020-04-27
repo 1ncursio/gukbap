48da204a2c69ee14eac61aeb6be94982d6203c10
1ncursio <ckswn1323@naver.com>
2020-04-27 23:09:17 +0900

    !

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const form = document.querySelector('.form'),
 
 function calcPrice(p) {
   result = parseInt(p / gukbapPrice.value);
-  description.textContent = `야! 그런 걸 왜 사냐? 그 돈이면 뜨끈~하고 든든~한 국밥 ${result}그릇은 사먹을 수 있겠다.`;
+  description.textContent = `야! 그런 걸 왜 사냐? 그 돈이면 뜨끈~하고 든든~한 국밥 ${result}그릇은 사먹을 수 있겠다!`;
   description.classList.toggle('showing');
 }
 
